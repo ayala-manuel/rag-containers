640f33df9a99453a340bdf0b4e3b121c8b8bbb01
ayalamanu <manu.ayala05@gmail.com>
2025-07-09 18:51:10 +0200

    Embedding fixed

--- utils/embedding_client.py
+++ utils/embedding_client.py
@@ -1,25 +1,12 @@
 # utils/embedding_client.py
 
-from pydantic import BaseModel
-from typing import List
 import httpx
 
-class EmbeddingsRequest(BaseModel):
-    texts: List[str]
+EMBEDDINGS_SERVICE_URL = "http://embeddings_service:8001/embed"
 
-class EmbeddingsResponse(BaseModel):
-    embeddings: List[List[float]]
-
-http_client = httpx.AsyncClient(timeout=10.0)
-
-async def embed_texts(request: EmbeddingsRequest):
-    url = "http://embeddings_service:8001/embed"
+async def get_embeddings(texts: list[str]) -> list[list[float]]:
     async with httpx.AsyncClient(timeout=10) as client:
-        try:
-            response = await client.post(url, json={"texts": request.texts})
-            response.raise_for_status()
-            data = response.json()
-            response =  EmbeddingsResponse(embeddings=data.get("embeddings", []))
-            return response["embeddings"][0]
-        except httpx.RequestError as e:
-            return {"error": f"Error connecting to embeddings_service: {str(e)}"}
+        response = await client.post(EMBEDDINGS_SERVICE_URL, json={"texts": texts})
+        response.raise_for_status()
+        data = response.json()
+        return data.get("embeddings", [])
